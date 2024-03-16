1f265e51ec5c325790df1af6e88696e1ee028fd6
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-03-16 21:44:01 +0530

    Github release added

--- src/main/java/com/bytegen/enums/ClaimType.java
+++ src/main/java/com/bytegen/enums/ClaimType.java
@@ -1,4 +1,4 @@
-package io.github.sachithariyathilaka.enums;
+package com.bytegen.enums;
 
 /**
  * @enum ClaimType
--- src/main/java/com/bytegen/filter/JwtRequestFilter.java
+++ src/main/java/com/bytegen/filter/JwtRequestFilter.java
@@ -1,7 +1,7 @@
-package io.github.sachithariyathilaka.filter;
+package com.bytegen.filter;
 
-import io.github.sachithariyathilaka.enums.ClaimType;
-import io.github.sachithariyathilaka.util.JwtTokenUtil;
+import com.bytegen.enums.ClaimType;
+import com.bytegen.util.JwtTokenUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
